6f95d1a9c50f8c7bf479e19f812b23a425b39f41
Philip Martel <pmartel@alum.mit.edu>
2018-05-06 12:52:00 -0400

    Adding stuff to guesser.js
    adding final CRLF to sudoku.html
    adding to-do list

--- guesser.js
+++ guesser.js
@@ -9,6 +9,11 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."'
 */
 
+// a guess is an object with a row, column and value
+var guess = {};
+
+// the gameStack has a boardString a guess and a "pointer" to the previous stack element 
+var gameStack = {};
 
 function guesser() {
 	var r,c;
@@ -16,8 +21,14 @@ function guesser() {
 	generatePossibleCells();
 	if( clearBoardUsed()){
 		// filled
-		
+		return;
 	}
+	pushGame();
+	
+}
+
+//function 
+function pushGame() {
 	
 }
 
